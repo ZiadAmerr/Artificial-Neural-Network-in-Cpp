6400b098ddb0fa53cf3c30fd8be680c2c6f8f670
ZiadAmerr <xeiad.amerr@yahoo.com>
2022-12-05 17:20:40 +0200

    Used .cpp and .hpp files

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -2,157 +2,103 @@
 //  NeuralNetwork.cpp
 //  Project
 //
-//  Created by Ziad Amerr on 04/12/2022.
+//  Created by Ziad Amerr on 05/12/2022.
 //
 #include <iostream>
 #include <vector>
+#include <math.h>
+#include "NeuralNetwork.hpp"
+#include "NeuralNetwork.hpp"
 using namespace std;
 
-class NeuralNetwork {
-private:
-    class Arrow {
-    public:
-        Arrow(int nextNeuronId, double weight=0) {
-            this->nextNeuronId = nextNeuronId;
-            this->weight = weight;
-        }
-        
-        void setWeight(double w) {
-            weight = w;
-        }
-        
-        double getWeight() {
-            return weight;
-        }
-        
-        int getNextNeuronId() {
-            return nextNeuronId;
-        }
 
-    private:
-        int nextNeuronId;
-        double weight;
-    };
+bool NeuralNetwork::_checkIfNeuronExists(int neuronId, int layerIdx) {
+    for(int i = 0; i<neurons.size(); i++)
+        if(neurons.at(layerIdx).at(i).getNeuronId() == neuronId)
+            return true;
     
-    class Neuron {
-    public:
-        Neuron() {
-            neuronId = 0;
-            layerId = 0;
-            activation = 0;
-        }
-        
-        Neuron(int neuronId, int layerId) {
-            this->neuronId = neuronId;
-            this->layerId = layerId;
-            this->activation = 0;
-        }
-        
-        ~Neuron() {}
-        
-        int getNeuronId() {
-            return neuronId;
-        }
-        
-        vector<Arrow> getArrowList() {
-            return arrowList;
-        }
-        
-        void addArrow(int nextNeuronId) {
-            Arrow myArrow = Arrow(nextNeuronId, 0);
-            arrowList.push_back(myArrow);
-        }
-        
-        void printArrowList() {
-            cout << '[';
-            for(auto it = arrowList.begin(); it != arrowList.end(); it++)
-                cout << "N#" << it->getNextNeuronId() << '(' << it->getWeight() <<"), ";
-            cout << ']' << endl;
-        }
-        
-        int getLayerId() {
-            return layerId;
-        }
+    // If not found
+    return false;
+}
 
-    private:
-        int neuronId, layerId;
-        double activation;
-        vector<Arrow> arrowList;
-    };
+NeuralNetwork::Neuron NeuralNetwork::_getNeuron(int neuronId, int layerIdx) {
+    Neuron n;
+    for (int i = 0; i < neurons.size(); i++) {
+        n = neurons.at(layerIdx).at(i);
+        if (n.getNeuronId() == neuronId)
+            return n;
+    }
     
-    vector<vector<Neuron>> neurons;
-    
-    bool _checkIfNeuronExists(int neuronId, int layerIdx) {
-        for(int i = 0; i<neurons.size(); i++)
-            if(neurons.at(layerIdx).at(i).getNeuronId() == neuronId)
-                return true;
+    // If not found
+    cout << "Error! Neuron not found!" << endl;
+    return n;
+}
+
+void NeuralNetwork::_initializeLayer(int numberOfNeurons, int layerIdx) {
+    vector<Neuron> myVec;
+    Neuron newNeuron;
+    for(int i=0; i<numberOfNeurons; i++) {
+        Neuron newNeuron = Neuron(i, layerIdx);
+        myVec.push_back(newNeuron);
+    }
+    neurons.push_back(myVec);
+}
+
+void NeuralNetwork::_connectLayers(int firstLayerId) {
+    // Loop through the current layers neurons
+    for(int i=0; i<neurons.at(firstLayerId).size(); i++) {
         
-        // If not found
-        return false;
-    }
-    
-    Neuron _getNeuron(int neuronId, int layerIdx) {
-        Neuron n;
-        for (int i = 0; i < neurons.size(); i++) {
-            n = neurons.at(layerIdx).at(i);
-            if (n.getNeuronId() == neuronId)
-                return n;
-        }
-        
-        // If not found
-        cout << "Error! Neuron not found!" << endl;
-        return n;
-    }
-    
-    void _initializeLayer(int numberOfNeurons, int layerIdx) {
-        vector<Neuron> myVec;
-        Neuron newNeuron;
-        for(int i=0; i<numberOfNeurons; i++) {
-            Neuron newNeuron = Neuron(i, layerIdx);
-            myVec.push_back(newNeuron);
-        }
-        neurons.push_back(myVec);
-    }
-    
-    void _connectLayers(int firstLayerId) {
-        // Loop through the current layers neurons
-        for(int i=0; i<neurons.at(firstLayerId).size(); i++) {
+        // Connect each neuron with ALL the neurons in the next layer
+        for(int j=0; j<neurons.at(firstLayerId+1).size(); j++) {
             
-            // Connect each neuron with ALL the neurons in the next layer
-            for(int j=0; j<neurons.at(firstLayerId+1).size(); j++) {
-                neurons.at(firstLayerId).at(i).addArrow(neurons.at(firstLayerId+1).at(j).getNeuronId());
-            };
-        }
-    };
-    
-public:
-    NeuralNetwork(vector<int> neuronsPerLayer) {
-        for(int i=0; i<neuronsPerLayer.size(); i++)
-            _initializeLayer(neuronsPerLayer.at(i), i);
-        
-        for(int i=0; i<neuronsPerLayer.size()-1; i++)
-            _connectLayers(i);
+            // currentNeuron.addArrow(otherNeuron.getId())
+            neurons.at(firstLayerId).at(i).addArrow(neurons.at(firstLayerId+1).at(j).getNeuronId());
+        };
     }
-    
-    void printNetwork() {
-        Neuron n;
-        for(int i=0; i<neurons.size(); i++) {
-            cout << "Layer " << i << endl;
-            for(int j=0; j<neurons.at(i).size(); j++) {
-                n = neurons.at(i).at(j);
-                cout << "  N#" << n.getNeuronId() << "-->";
-                n.printArrowList();
-            }
-            cout << endl;
-        }
-    }
-    
-    // Prints summary of the network, n_layers, n_neurons per layer
-    // void printSummary();
-    
-    // Load pretrained model from JSON file
-    // void loadWeightsFromFile(JSONFile file);
-    
-    // Infer model on image and produce output, returns the most confident integer
-    // int infer(Image img);
 };
+
+NeuralNetwork::NeuralNetwork(vector<int> neuronsPerLayer) {
+    for(int i=0; i<neuronsPerLayer.size(); i++)
+        _initializeLayer(neuronsPerLayer.at(i), i);
+    
+    for(int i=0; i<neuronsPerLayer.size()-1; i++)
+        _connectLayers(i);
+}
+
+void NeuralNetwork::printNetwork(string detailed) {
+    NeuralNetwork::Neuron n;
+    for(int i=0; i<neurons.size(); i++) {
+        cout << "Layer " << i << endl;
+        for(int j=0; j<neurons.at(i).size(); j++) {
+            n = neurons.at(i).at(j);
+            cout << "  N#" << n.getNeuronId();
+            
+            
+        
+            if(detailed == "detailed" and i!=neurons.size()-1) {
+                cout << "-->";
+                n.printArrowList();
+            } else {
+                cout << endl;
+            }
+        }
+        cout << endl;
+    }
+}
+
+
+// ************************* PHASE 2 *************************
+// Prints summary of the network, n_layers, n_neurons per layer
+// void printSummary();
+
+// ************************* PHASE 2 *************************
+// Load pretrained model from JSON file
+// void loadWeightsFromFile(JSONFile file);
+
+// ************************* PHASE 2 *************************
+// Infer model on image and produce output, returns the most confident integer
+// int infer(Image img);
+
+
+
+
--- Project/main.cpp
+++ Project/main.cpp
@@ -6,20 +6,23 @@
 //
 
 #include <iostream>
-#include "../NeuralNetwork.cpp"
+//#include "../NeuralNetwork.cpp"
+//#include "../Activations.cpp"
+#include "NeuralNetwork.hpp"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     vector<int> NeuralNetworkParams = {
-        28,
-        4,
-        4,
-        1
+        16,
+        8,
+        4
     };
     
     NeuralNetwork myNetwork = NeuralNetwork(NeuralNetworkParams);
     
-    myNetwork.printNetwork();
+    myNetwork.printNetwork("detailed");
+    
+    //cout << NeuralNetwork::Activations::Sigmoid(0.5) << endl;
     
     return 0;
 }
